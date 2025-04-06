9ae1dcc0621df4bbcaa044589a06fb0fab0016d5
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-04-06 10:30:41 +0500

    Country section completed

--- src/Pages/Country.jsx
+++ src/Pages/Country.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { countries } from '../mock/countries';
 
 const Country = () => {
-  return (
-    <div>C</div>
-  )
-}
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
 
-export default Country
+  return (
+    <section id="country">
+      <div className='container mx-auto px-4 w-[1300px]  mb-[100px]'>
+        <div data-aos="fade-right">
+          <h1 className='text-[40px] font-bold mt-[50px]'>Mamlakatlar</h1>
+          <div className='w-[50px] h-[5px] bg-[#ff0000] mb-[20px]'></div>
+        </div>
+
+        <div className='flex flex-wrap gap-[30px] justify-center xl:justify-between mt-[40px] aos-init aos-animate'
+          data-aos="zoom-in">
+          {
+            countries.map((country, index) => (
+              <div className='shadow-lg max-w-[350px] h-[282px] hover:cursor-pointer w-full rounded-t-[32px] '
+                key={country.id}
+                data-aos={index % 2 === 0 ? 'fade-up' : 'fade-down'}
+              >
+                <div className='w-full h-[200px]'>
+                  <img src={country.img} alt={country.name}
+                    className='w-full h-[200px] rounded-t-2xl' />
+                </div>
+                <div className='relative flex'>
+                  <h3 className='px-[20px] text-[22px] font-bold uppercase my-[25px]'>
+                    {country.name}
+                  </h3>
+                  <div className='absolute right-2 top-5'>
+                    <img src={country.logo} alt={`${country.name} logo`} className='mx-auto w-[80px] h-[50px] rounded-2xl shadow-lg' />
+                  </div>
+                </div>
+                <div className='w-full h-[3px] bg-[#ff0000]'></div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Country;
---
+++ src/mock/countries.jsx
@@ -0,0 +1,76 @@
+import img1 from '../assets/country1.jpg';
+import img2 from '../assets/country2.jpeg';
+import img3 from '../assets/country3.jpg';
+import img4 from '../assets/country4.jpg';
+import img5 from '../assets/country5.jpg';
+import img6 from '../assets/country6.jpg';
+import img7 from '../assets/country7.jpg';
+import img8 from '../assets/country8.jpg';
+import img9 from '../assets/country9.jpg';
+
+import logo1 from '../assets/C-logo1.png';
+import logo2 from '../assets/C-logo2.png';
+import logo3 from '../assets/C-logo3.jpg';
+import logo4 from '../assets/C-logo4.jpg';
+import logo5 from '../assets/C-logo5.jpg';
+import logo6 from '../assets/C-logo6.png';
+import logo7 from '../assets/C-logo7.jpg';
+import logo8 from '../assets/C-logo8.jpg';
+import logo9 from '../assets/C-logo9.png';
+
+export const countries = [
+  {
+    id: 1,
+    name: "Shengen",
+    img: img1,
+    logo: logo1
+  },
+  {
+    id: 2,
+    name: "Kanada",
+    img: img2,
+    logo: logo2
+  },
+  {
+    id: 3,
+    name: "Aqsh",
+    img: img3,
+    logo: logo3
+  },
+  {
+    id: 4,
+    name: "Buyuk Britaniya",
+    img: img4,
+    logo: logo4
+  },
+  {
+    id: 5,
+    name: "Avstraliya",
+    img: img5,
+    logo: logo5
+  },
+  {
+    id: 6,
+    name: "Yangi Zelandiya",
+    img: img6,
+    logo: logo6
+  },
+  {
+    id: 7,
+    name: "Yaponiya",
+    img: img7,
+    logo: logo7
+  },
+  {
+    id: 8,
+    name: "Koreya",
+    img: img8,
+    logo: logo8
+  },
+  {
+    id: 9,
+    name: "Lotin Amerikasi",
+    img: img9,
+    logo: logo9
+  }
+];
