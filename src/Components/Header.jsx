fbc08111755f9ace131ae1b084e7081bea652d36
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-04-19 19:32:41 +0500

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,46 +10,39 @@ const Header = () => {
   };
 
   return (
-    <header  className="fixed top-0 left-0 w-full z-50">
+    <header className="fixed top-0 left-0 w-full z-50">
       <div className='bg-white shadow-md' >
-        <nav className='relative'>
-          <img className='max-w-[800px] h-full opacity-25 absolute left-1/2 top-1/2 transform translate-x-1/2 -translate-y-1/2' src={navBg} alt="navbar-bg" />
-          <div className='w-[1300px] mx-auto h-[80px] px-[15px] mt-[10px] py-2 flex justify-between'>
-            <a className='w-[100px] h-[60px]' href="/">
-              <img src={logo} alt="logotip" />
-            </a>
-            <ul className='items-center flex uppercase font-bold text-[14px] gap-8 ml-auto mr-[30px]'>
-              <li>
-                <a className='hover:text-red-600' href="about">{t('header.about')}</a>
-              </li>
-              <li>
-                <a className='hover:text-red-600' href="/country">{t('header.country')}</a>
-              </li>
-              <li>
-                <a className='hover:text-red-600' href="/service">{t('header.service')}</a>
-              </li>
-              <li>
-                <a className='hover:text-red-600' href="/work">{t('header.work')}</a>
-              </li>
-              <li>
-                <a className='hover:text-red-600' href="/contact">{t('header.contact')}</a>
-              </li>
-              <select
-                className='uppercase bg-black text-white p-1'
-                name="lang"
-                id="lang"
-                onChange={(e) => changeLanguage(e?.target?.value)}
-                value={i18n.language}
-              >
-                <option className='uppercase' value="uz">Uz</option>
-                <option className='uppercase' value="ru">Ru</option>
-                <option className='uppercase' value="en">En</option>
-              </select>
-            </ul>
-          </div>
+      <nav className='relative bg-cover bg-center'>
+  <img
+    src={navBg}
+    alt="navbar-bg"
+    className='w-[800px] absolute right-50 top-0 h-full opacity-25 z-0'
+  />
+  <div className='w-[1300px] mx-auto h-[80px] px-[15px] mt-[10px] py-2 flex justify-between relative z-10'>
+    <a className='w-[100px] h-[60px]' href="/">
+      <img src={logo} alt="logotip" />
+    </a>
+    <ul className='items-center flex uppercase font-bold text-[14px] gap-8 ml-auto mr-[30px]'>
+      <li><a className='hover:text-red-600' href="about">{t('header.about')}</a></li>
+      <li><a className='hover:text-red-600' href="/country">{t('header.country')}</a></li>
+      <li><a className='hover:text-red-600' href="/service">{t('header.service')}</a></li>
+      <li><a className='hover:text-red-600' href="/work">{t('header.work')}</a></li>
+      <li><a className='hover:text-red-600' href="/contact">{t('header.contact')}</a></li>
+      <select
+        className='uppercase bg-black text-white p-1'
+        name="lang"
+        id="lang"
+        onChange={(e) => changeLanguage(e?.target?.value)}
+        value={i18n.language}
+      >
+        <option className='uppercase' value="uz">Uz</option>
+        <option className='uppercase' value="ru">Ru</option>
+        <option className='uppercase' value="en">En</option>
+      </select>
+    </ul>
+  </div>
+</nav>
 
-
-        </nav>
       </div>
     </header>
   );
